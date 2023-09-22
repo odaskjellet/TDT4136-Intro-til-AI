bb9aa25c69bcaa4d1a8be20d21e2e69429d2ec2a
omizoe <oda.skjellet@gmail.com>
2023-09-22 10:44:21 +0200

    Added search code

--- assignment_2/Astar.py
+++ assignment_2/Astar.py
@@ -1,8 +1,58 @@
 from Map import Map_Obj 
 
+
+#Djiktras
+
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements: list[tuple[float, T]] = []
+    
+    def empty(self) -> bool:
+        return not self.elements
+    
+    def put(self, item: T, priority: float):
+        heapq.heappush(self.elements, (priority, item))
+    
+    def get(self) -> T:
+        return heapq.heappop(self.elements)[1]
+
+
+class WeightedGraph(Graph):
+    def cost(self, from_id: Location, to_id: Location) -> float: pass
+
+
+
 def man_dis(a: list, b: list) -> float: #FOR THE FIRST PART, should be simple
         #manhatten distance
         x1, y1 = a[0], a[1]
         x2, y2 = b[0], b[1]
         return abs(x1 - x2) - abs(y1-y2)
+
+
+
+def a_star_search(graph: WeightedGraph, start: Location, goal: Location):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from: dict[Location, Optional[Location]] = {}
+    cost_so_far: dict[Location, float] = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    
+    while not frontier.empty():
+        current: Location = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + man_dis(next, goal)
+                frontier.put(next, priority)
+                came_from[next] = current
+    
+    return came_from, cost_so_far
 
